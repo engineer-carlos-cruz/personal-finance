096e2b77e8f2bfb897bf78f2e863b67dde0623b8
carlos cruz <carturo99@gmail.com>
2026-05-26 21:20:52 -0500

    feat(income-category): add income category entity

---
+++ src/main/java/com/ccruz/personal_finance/income_category/persistence/IncomeCategory.java
@@ -0,0 +1,42 @@
+package com.ccruz.personal_finance.income_category.persistence;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "income_categories")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class IncomeCategory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
+    @Column(name = "id", nullable = false, updatable = false)
+    private Long id;
+
+    @NotNull
+    @Size(max = 100)
+    @Column(name = "name", nullable = false, length = 100)
+    private String name;
+
+    @Size(max = 255)
+    @Column(name = "description", length = 255)
+    private String description;
+}
