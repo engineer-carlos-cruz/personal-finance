11d098379d48f2b78006431c506a99be36411c5b
carlos cruz <carturo99@gmail.com>
2026-06-07 16:58:35 -0500

    feat: add @PositiveOrZero validation on balance field

--- src/main/java/com/ccruz/personal_finance/account/web/dto/AccountUpsertRequest.java
+++ src/main/java/com/ccruz/personal_finance/account/web/dto/AccountUpsertRequest.java
@@ -3,6 +3,7 @@ package com.ccruz.personal_finance.account.web.dto;
 import jakarta.validation.constraints.Digits;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.PositiveOrZero;
 import jakarta.validation.constraints.Size;
 
 import java.math.BigDecimal;
@@ -17,6 +18,7 @@ public record AccountUpsertRequest(
     String description,
     
     @NotNull
+    @PositiveOrZero
     @Digits(integer = 17, fraction = 2)
     BigDecimal balance
 ) {
