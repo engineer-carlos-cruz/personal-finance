189da849c79983108a9f79138d4625738956a876
carlos cruz <carturo99@gmail.com>
2026-06-12 10:24:34 -0500

    feat(account): add findAllIncludingInactive query to AccountRepository

--- src/main/java/com/ccruz/personal_finance/account/persistence/AccountRepository.java
+++ src/main/java/com/ccruz/personal_finance/account/persistence/AccountRepository.java
@@ -1,11 +1,16 @@
 package com.ccruz.personal_finance.account.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface AccountRepository extends JpaRepository<Account, Long> {
     
     Optional<Account> findByCode(String code);
     boolean existsByCode(String code);
+
+    @Query(value = "SELECT * FROM accounts", nativeQuery = true)
+    List<Account> findAllIncludingInactive();
 }
