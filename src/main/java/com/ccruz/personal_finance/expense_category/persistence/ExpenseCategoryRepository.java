af4671ae21d8f720006799da70904a4573a36e98
carlos cruz <carturo99@gmail.com>
2026-06-13 20:12:01 -0500

    feat: add findAllIncludingInactive query to ExpenseCategoryRepository
    Add a native query method on ExpenseCategoryRepository that retrieves all expense categories including inactive ones, bypassing the default filtering.

--- src/main/java/com/ccruz/personal_finance/expense_category/persistence/ExpenseCategoryRepository.java
+++ src/main/java/com/ccruz/personal_finance/expense_category/persistence/ExpenseCategoryRepository.java
@@ -1,6 +1,12 @@
 package com.ccruz.personal_finance.expense_category.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ExpenseCategoryRepository extends JpaRepository<ExpenseCategory, Long> {
+
+    @Query(value = "SELECT * FROM expense_categories", nativeQuery = true)
+    List<ExpenseCategory> findAllIncludingInactive();
 }
