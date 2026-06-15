e7135e6858e3564a64e8e054391427ffc884ee97
carlos cruz <carturo99@gmail.com>
2026-06-15 15:03:58 -0500

    feat(income-category): add native query to find all categories including inactive

--- src/main/java/com/ccruz/personal_finance/income_category/persistence/IncomeCategoryRepository.java
+++ src/main/java/com/ccruz/personal_finance/income_category/persistence/IncomeCategoryRepository.java
@@ -1,6 +1,12 @@
 package com.ccruz.personal_finance.income_category.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface IncomeCategoryRepository extends JpaRepository<IncomeCategory, Long> {
+
+    @Query(value = "SELECT * FROM income_categories", nativeQuery = true)
+    List<IncomeCategory> findAllIncludingInactive();
 }
