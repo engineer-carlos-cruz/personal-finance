4dfdb8a46fb5517445735a5b11752ef2ee44713e
carlos cruz <carturo99@gmail.com>
2026-06-27 22:31:57 -0500

    feat: add findAllIncludingInactive query to IncomeRepository
    Add a native query method to bypass the @SQLRestriction filter when inactive incomes need to be included.

--- src/main/java/com/ccruz/personal_finance/income/persistence/IncomeRepository.java
+++ src/main/java/com/ccruz/personal_finance/income/persistence/IncomeRepository.java
@@ -1,6 +1,12 @@
 package com.ccruz.personal_finance.income.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface IncomeRepository extends JpaRepository<Income, Long> {
+
+    @Query(value = "SELECT * FROM incomes", nativeQuery = true)
+    List<Income> findAllIncludingInactive();
 }
