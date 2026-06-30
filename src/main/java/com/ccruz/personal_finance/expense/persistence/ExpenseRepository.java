e9cdc053fa595d1705b41e2d46f75b1f2733254a
carlos cruz <carturo99@gmail.com>
2026-06-29 21:59:00 -0500

    feat(expense): add findAllIncludingInactive native query to bypass soft-delete filter

--- src/main/java/com/ccruz/personal_finance/expense/persistence/ExpenseRepository.java
+++ src/main/java/com/ccruz/personal_finance/expense/persistence/ExpenseRepository.java
@@ -1,6 +1,12 @@
 package com.ccruz.personal_finance.expense.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+
+    @Query(value = "SELECT * FROM expenses", nativeQuery = true)
+    List<Expense> findAllIncludingInactive();
 }
